cc0471f74d15762d55d7ce7cece20d0669146222
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-09-15 16:38:55 +0100

    outputting cart items

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
+import CartContext from "../../Store/cart-context";
 
 const Cart = (props) => {
+  const cartsCtx = useContext(CartContext);
+  const totalAmount = `$${cartsCtx.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartsCtx.meals.length > 0;
   const cartItems = (
     <ul>
-      {[{ id: "c1", name: "Sushi", Amount: 2, price: 12.99 }].map((item) => (
+      {cartsCtx.meals.map((item) => (
         <li key={item.id}>{item.name} </li>
       ))}
     </ul>
   );
+
   return (
     <Modal onClose={props.onHideCart}>
       {cartItems}
+
       <div className="flex justify-between items-center text-[1.5rem] my-[1rem] mx-0">
         <span className="text-2xl font-bold">Total Amount</span>
-        <span className="text-2xl font-bold">35.62</span>
+        <span className="text-2xl font-bold">{totalAmount}</span>
       </div>
       <div className="text-right">
         <button
@@ -23,9 +30,11 @@ const Cart = (props) => {
         >
           Close
         </button>
-        <button className="border-[#8a2b06] border-2 bg-transparent cursor-pointer font-inherit rounded-2xl ml-[1rem] py-[0.5rem] px-[2rem] hover:bg-[#5a1a01] hover:border-[#5a1a01] hover:text-white active:bg-[#5a1a01] active:border-[#5a1a01] active:text-white text-[#8a2b06]">
-          Order
-        </button>
+        {hasItems && (
+          <button className="border-[#8a2b06] border-2 bg-transparent cursor-pointer font-inherit rounded-2xl ml-[1rem] py-[0.5rem] px-[2rem] hover:bg-[#5a1a01] hover:border-[#5a1a01] hover:text-white active:bg-[#5a1a01] active:border-[#5a1a01] active:text-white text-[#8a2b06]">
+            Order
+          </button>
+        )}
       </div>
     </Modal>
   );
